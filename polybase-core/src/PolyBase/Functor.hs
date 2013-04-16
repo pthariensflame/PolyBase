aa4504e4292edd1eb0cca7b7a33dedf8bb02a024
Alexander Altman <alexanderaltman@me.com>
2013-04-16 16:32:43 -0700

    fix
    [none]

--- polybase-core/src/PolyBase/Functor.hs
+++ polybase-core/src/PolyBase/Functor.hs
@@ -23,5 +23,5 @@ contramap = fmap . Op
 contramap' :: forall (f :: k1 -> k2) (a :: k1) (b :: k1) (cat1 :: k1 -> k1 -> *) (cat2 :: k2 -> k2 -> *). (Functor f, cat1 ~ FunctorC1 f, Op cat2 ~ FunctorC2 f) => cat1 a b -> cat2 (f b) (f a)
 contramap' = runOp . fmap
 
-fmap' :: forall (a :: k1) (b :: k1) (cat1 :: k1 -> k1 -> *) (cat2 :: k2 -> k2 -> *). (cat1 ~ FunctorC1 f, cat2 ~ FunctorC2 f) => cat1 b a -> cat2 (f b) (f a)
+fmap' :: forall (f :: k1 -> k2) (a :: k1) (b :: k1) (cat1 :: k1 -> k1 -> *) (cat2 :: k2 -> k2 -> *). (Functor f, Op cat1 ~ FunctorC1 f, Op cat2 ~ FunctorC2 f) => cat1 b a -> cat2 (f b) (f a)
 fmap' = runOp . fmap . Op
