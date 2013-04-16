9c150f3067ae7d29559372408258d9e3dfe90188
Alexander Altman <alexanderaltman@me.com>
2013-04-16 15:55:49 -0700

    Fixed formatting
    (plus some other stuff)

--- polybase-core/src/PolyBase/Category.hs
+++ polybase-core/src/PolyBase/Category.hs
@@ -1,20 +1,26 @@
-{-# LANGUAGE PolyKinds, TypeOperators, KindSignatures, ScopedTypeVariables, GADTs #-}
+{-# LANGUAGE PolyKinds, TypeOperators, KindSignatures, ScopedTypeVariables, RankNTypes, GADTs #-}
 module PolyBase.Category (Category(..),
-                          (:~)(..)) where
+                          (:~)(..),
+                          Op(..)) where
 import Prelude hiding ((.), id)
 import qualified Prelude as P
 
 class Category (cat :: k -> k -> *) where
-    (.) :: forall (a :: k) (b :: k) (c :: k). cat b c -> cat a b -> cat a c
-    id :: forall (a :: k). cat a a
+  (.) :: forall (a :: k) (b :: k) (c :: k). cat b c -> cat a b -> cat a c
+  id :: forall (a :: k). cat a a
 
 instance Category (->) where
-	(.) = (P..)
-    id = P.id
+  (.) = (P..)
+  id = P.id
 
 data (:~) :: k -> k -> * where
-    Refl :: forall (a :: k). a :~ a
+  Refl :: forall (a :: k). a :~ a
 
 instance Category (:~) where
-    Refl . Refl = Refl
-    id = Refl
+  Refl . Refl = Refl
+  id = Refl
+
+data Op :: (k -> k -> *) -> k -> k -> * where
+  Op :: forall (cat :: k -> k -> *) (a :: k) (b :: k). { getCat :: cat b a } -> Op cat a b
+
+
--- polybase-core/src/PolyBase/Functor.hs
+++ polybase-core/src/PolyBase/Functor.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE PolyKinds, TypeOperators, KindSignatures, ScopedTypeVariables, GADTs, TypeFamilies #-}
 module PolyBase.Functor (Functor(..),
-                         Contravariant(..)) where
+                         contramap) where
 import Prelude hiding ((.), id, Functor(..))
 import PolyBase.Category
 import PolyBase.Indexed
 
 class (Category (FunctorC1 f), Category (FunctorC2 f)) => Functor (f :: k1 -> k2) where
-    type FunctorC1 f :: k1 -> k1 -> *
-    type FunctorC2 f :: k2 -> k2 -> *
-    fmap :: forall (a :: k1) (b :: k1) (cat1 :: k1 -> k1 -> *) (cat2 :: k2 -> k2 -> *). (cat1 ~ FunctorC1 f, cat2 ~ FunctorC2 f) => cat1 a b -> cat2 (f a) (f b)
+  type FunctorC1 f :: k1 -> k1 -> *
+  type FunctorC2 f :: k2 -> k2 -> *
+  fmap :: forall (a :: k1) (b :: k1) (cat1 :: k1 -> k1 -> *) (cat2 :: k2 -> k2 -> *). (cat1 ~ FunctorC1 f, cat2 ~ FunctorC2 f) => cat1 a b -> cat2 (f a) (f b)
 
 instance Functor ((a :: *) := (i :: ki) :: ki -> *) where
-    type FunctorC1 (a := i) = (:~)
-    type FunctorC2 (a := i) = (->)
-    fmap Refl = id
-    
+  type FunctorC1 (a := i) = (:~)
+  type FunctorC2 (a := i) = (->)
+  fmap Refl = id
+
+contramap :: forall (f :: k1 -> k2) (a :: k1) (b :: k1) (cat1 :: k1 -> k1 -> *) (cat2 :: k2 -> k2 -> *). (Functor f, Op cat1 ~ FunctorC1 f, cat2 ~ FunctorC2 f) => cat1 b a -> cat2 (f a) (f b)
+contramap = fmap . Op
