d39e4c7383db6f021669002fd26b1d78e1d7ef33
Alexander Altman <alexanderaltman@me.com>
2013-04-16 20:36:02 -0700

    func insts
    [none]

--- polybase-core/src/PolyBase/Category.hs
+++ polybase-core/src/PolyBase/Category.hs
@@ -4,8 +4,9 @@ module PolyBase.Category (Category(..),
                           Op(..),
                           HasProduct(..),
                           HasUnit(..)) where
-import Prelude hiding ((.), id, fst, snd, swap)
+import Prelude hiding ((.), id, fst, snd)
 import qualified Prelude as P
+import qualified Data.Tuple as T (swap)
 
 class Category (cat :: k -> k -> *) where
   (.) :: forall (a :: k) (b :: k) (c :: k). cat b c -> cat a b -> cat a c
@@ -36,9 +37,22 @@ class (Category cat) => HasProduct (cat :: k -> k -> *) where
   snd = fst . swap
   swap :: forall (prod :: k -> k -> k) (a :: k) (b :: k). (prod ~ Product cat) => cat (prod a b) (prod b a)
 
+-- | Product = (,)
+instance HasProduct (->) where
+  type Product (->) = (,)
+  fst = P.fst
+  snd = P.snd
+  swap = T.swap
+
 class (HasProduct cat) => HasUnit (cat :: k -> k -> *) where
   type Unit cat :: k
   unitL :: forall (prod :: k -> k -> k) (unit :: k) (a :: k). (prod ~ Product cat, unit ~ Unit cat) => cat a (prod unit a)
   unitL = swap . unitR
   unitR :: forall (prod :: k -> k -> k) (unit :: k) (a :: k). (prod ~ Product cat, unit ~ Unit cat) => cat a (prod a unit)
   unitR = swap . unitL
+
+-- | Unit = ()
+instance HasUnit (->) where
+  type Unit (->) = ()
+  unitL v = ((), v)
+  unitR v = (v, ())
