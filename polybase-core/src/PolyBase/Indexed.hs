2e8d4d51b0f3dde70a6fed6ffcbf1db8828e114a
Alexander Altman <alexanderaltman@me.com>
2013-04-16 20:43:15 -0700

    reformat
    [none]

--- polybase-core/src/PolyBase/Indexed.hs
+++ polybase-core/src/PolyBase/Indexed.hs
@@ -6,13 +6,13 @@ import Prelude hiding ((.), id)
 import PolyBase.Category
 
 data Indexed :: (k -> k -> *) -> (ki -> k) -> (ki -> k) -> * where
-    Indexed :: forall (cat :: k -> k -> *) (a :: ki -> k) (b :: ki -> k). { runIndexed :: forall (i :: ki). cat (a i) (b i) } -> Indexed cat a b
+  Indexed :: forall (cat :: k -> k -> *) (a :: ki -> k) (b :: ki -> k). { runIndexed :: forall (i :: ki). cat (a i) (b i) } -> Indexed cat a b
 
 type (:->) = (Indexed (->) :: (ki -> *) -> (ki -> *) -> *)
 
 instance (Category cat) => Category (Indexed (cat :: k -> k -> *) :: (ki -> k) -> (ki -> k) -> *) where
-    Indexed x . Indexed y = Indexed (x . y)
-    id = Indexed id
+  Indexed x . Indexed y = Indexed (x . y)
+  id = Indexed id
 
 data (:=) :: * -> ki -> ki -> * where
-    AtKey :: forall (v :: *) (i :: ki). { getAtKey :: v } -> (v := i) i
+  AtKey :: forall (v :: *) (i :: ki). { getAtKey :: v } -> (v := i) i
